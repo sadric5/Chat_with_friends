24b1ac78e8fdd3c30a2a05cfd5ee65eb3666070b
Sadric Adigbe <sadricgamer@gmail.com>
2021-04-08 14:11:14 -0600

    style the register page and the url route

--- users_handler/urls.py
+++ users_handler/urls.py
@@ -5,8 +5,9 @@ import re
 
 
 urlpatterns = [
-	path('', views.user_register, name='user-register'),
+	path('', views.user_login, name='login-to-home'),
 	path('login', views.user_login, name='login-to-home'),
+	path('register', views.user_register, name='user-register'),
 	path('logout', views.user_logout, name='logout-to-home'),
 	path("profile/", views.userProfile, name='user-profile'),
 ]
