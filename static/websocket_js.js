8fb479c8b2f13df635f517fda2b22e6dedb3034b
Sadric Adigbe <sadricgamer@gmail.com>
2021-04-11 17:00:26 -0600

    Update websocket_js.js for ws to wws socket

--- static/websocket_js.js
+++ static/websocket_js.js
@@ -1,11 +1,19 @@
+
 var username = JSON.parse(document.getElementById('room-name').textContent);
-	const chat_socket = new WebSocket(
-		'ws://'
+		
+var protocl=window.location.protocol
+var protocol_use = protocl=="https:"?"wss":"ws";
+
+const chat_socket = new WebSocket(
+		protocol_use
+		+ "://"
 		+ window.location.host
-		+ '/ws/chat/'
+		+ ":8989"
+		+ '/wss/chat/'
 		+ username
 		+ '/'
-		)
+	)
+
 
 	message_section = $(".message-container");
 	p_element = document.createElement('div');
