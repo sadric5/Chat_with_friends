232b187e71b02b5837800629613aac129149f977
Sadric Adigbe <sadricgamer@gmail.com>
2021-04-25 15:57:39 -0600

    Update settings.py

--- chat_room/settings.py
+++ chat_room/settings.py
@@ -145,7 +145,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-# STATIC_URL = '/statics/'
+
+#Media file 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 #messegerie app settings
 #=========================================================
