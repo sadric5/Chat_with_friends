e9c0857fa06133a4f88aaccf7057329689d4b0e4
Sadric Adigbe <sadricgamer@gmail.com>
2021-04-26 05:08:13 +0000

    Update"

--- users_handler/apps.py
+++ users_handler/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class UsersHandlerConfig(AppConfig):
     name = 'users_handler'
-	def ready(self):
-		import users_handler.signals
+    def ready(self):
+        import users_handler.signals
