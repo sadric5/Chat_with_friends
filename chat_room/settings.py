4c321b715882438ee07fbb26da15ecb05b05e1cb
Sadric Adigbe <sadricgamer@gmail.com>
2021-05-02 01:36:28 -0600

    update

--- chat_room/settings.py
+++ chat_room/settings.py
@@ -153,6 +153,9 @@ STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+LOGIN_REDIRECT_URL = 'home-page'
+LOGIN_URL = 'login-to-home'
+
 #messegerie app settings
 #=========================================================
 ASGI_APPLICATION = 'chat_room.asgi.application'
