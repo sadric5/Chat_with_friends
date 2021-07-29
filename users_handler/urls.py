0fa33ef6e964ab543a9ea4f6f9e1916937ab66a2
sadric <sadricgamer@gmail.com>
2021-07-29 12:13:12 -0600

    update the second login page

--- users_handler/urls.py
+++ users_handler/urls.py
@@ -8,8 +8,8 @@ import re
 urlpatterns = [
 	path('settings/', views.users_settings, name='users-settings'),
 	path('', views.user_login, name='login-to-home'),
-	path('login', LoginView.as_view(template_name='login_page.html'), name='login-to-home'),
-	path('login2', views.user_login, name='login-to-home'),
+	path('login2', LoginView.as_view(template_name='login_page.html'), name='login-to-home'),
+	path('login', views.user_login, name='login-to-home'),
 	path('register', views.user_register, name='user-register'),
 	path('logout', views.user_logout, name='logout-to-home'),
 	path("profile/", views.userProfile, name='user-profile'),
